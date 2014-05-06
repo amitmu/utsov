c0457ffcec6db2750f3738233fd3f08a42c38a70
Amit@USBDrive <amitmu@outlook.com>
2014-05-06 00:28:35 -0400

    Updates fro layout and content

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 'use scrict';
 
-jQuery(window).load(function(){
+jQuery(window).load(function () {
     jQuery("#loader").fadeOut("slow");
 })
 
@@ -11,3 +11,38 @@ jQuery(window).load(function(){
       }
 });	*/
 
+jQuery(document).ready(function () {
+
+    // create a LatLng object containing the coordinate for the center of the map
+    var latlng = new google.maps.LatLng(40.566373, -74.382668);
+
+    // prepare the map properties
+    var options = {
+        zoom: 15,
+        center: latlng,
+        mapTypeId: google.maps.MapTypeId.ROADMAP,
+        navigationControl: true,
+        mapTypeControl: false,
+        scrollwheel: false,
+        disableDoubleClickZoom: true
+    };
+
+    // initialize the map object
+    var map = new google.maps.Map(document.getElementById('map_canvas'), options);
+
+    // add Marker
+    var marker1 = new google.maps.Marker({
+        position: latlng,
+        map: map
+    });
+
+    // add listener for a click on the pin
+    google.maps.event.addListener(marker1, 'click', function () {
+        infowindow.open(map, marker1);
+    });
+
+    // add information window
+    var infowindow = new google.maps.InfoWindow({
+        content: '<div class="info"><strong>Our Registered Office</strong><br/><br/>31 RICHARD ROAD,<br/> EDISON, NJ 08820.</div>'
+    });
+});
