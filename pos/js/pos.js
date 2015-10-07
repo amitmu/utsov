259da573e7e47af54153e7df016698340798d680
Amit USB <amitmu@outlook.com>
2015-10-07 00:00:15 -0400

    Admin feature added to POS

--- pos/js/pos.js
+++ pos/js/pos.js
@@ -1,32 +1,138 @@
 'use strict';
 
 var app = angular.module('utsovPOS', []);
+
+app.factory('userInfoService', function($rootScope){
+    
+    var userInfo = {};
+    
+    userInfo.type = 'GUEST';
+    userInfo.name = 'Patron';
+    
+    userInfo.updateUser = function(newUserType, newUserName){
+        this.type = newUserType;
+        this.name = newUserName;
+        this.publishUser();
+    };
+    
+    userInfo.publishUser = function() {
+        console.log("Broadcasting change from service...")
+        $rootScope.$broadcast('userChanged');    
+    };
+    
+    return userInfo;
+});
  
-app.controller('carouselCtrl', function ($scope, $http) {
+app.controller('carouselCtrl', function ($scope, $http, userInfoService) {
 	$scope.w = window.innerWidth;
 	$scope.h = window.innerHeight;
+    $scope.user = {};
+    $scope.user.type = 'GUEST';
+    $scope.user.name = 'Patron';
 	$scope.folders = [
 		'2012',
 		'2013',
 		'2014',
 	];
+    
 	$scope.images = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
 	$scope.currentFolder = $scope.folders[0];
 	$scope.selectFolder = function (folder) {
 		$scope.currentFolder = folder;
-	};
-	$scope.activeFolder = function (folder) {
+	}
+	
+    $scope.activeFolder = function (folder) {
 		return (folder === $scope.currentFolder) ? 'active' : '';
-	};
+	}    
+    
+    $scope.signOut = function(){
+        console.log("Signing out user...");
+        userInfoService.updateUser('GUEST', 'Patron');
+    }
+    
+    $scope.$on('userChanged', function(){
+        $scope.user.name = userInfoService.name;
+        $scope.user.type = userInfoService.type;
+        console.log("Trapping Event in carousel controller");
+        console.log("User:" + $scope.user.name + "[" + $scope.user.type + "]");
+    });
 });
 
 
-app.controller('registerCtrl', function ($scope, $http) {
+app.controller('loginCntrl', function ($scope, $http, userInfoService) {
+
+    $scope.errors = '';
+    $scope.msgs = '';
+    $scope.user = {};
+    $scope.user.type = 'GUEST';
+    $scope.title = "Admin Login";
+    $scope.service = '../api/status.php';
+    $scope.status = 0;
+    $scope.success = 0;
+
+    console.log("Action:" + $scope.action);
+    console.log("Service:" + $scope.service);
+    console.log("Title:" + $scope.title);
+
+    $scope.initialize = function(){
+        
+        $scope.formData = {};
+        $scope.success = 0;
+        $scope.errors = '';
+        $scope.msgs = '';
+        $scope.user = {};
+    }
+     //The login function
+    $scope.LoginUser = function () {
+        $scope.errors = '';
+        $scope.msgs = '';
+        $scope.success = 0;
+        $scope.formData.action = "login";
+        $http.post($scope.service,  $scope.formData
+        ).success(function(output, status, headers, config) {
+            if (output.err == ''){
+                $scope.msgs = "Server: " + output.msg;
+                $scope.success = 1;
+                $scope.user = output.data[0];
+                var newUserType = '';
+                var newUserName = '';
+                if($scope.user.utsov_access == 'W' || $scope.user.capture_access == 'W' || $scope.user.sponsor_access == 'W'){
+                    newUserType = 'ADMIN';
+                }
+                else{
+                    newUserType = 'USER';
+                }
+                newUserName = $scope.user.name;
+                
+                console.log($scope.msgs);
+                console.log("Raising Event in login controller");
+                console.log("User:" + newUserName + "[" + newUserType + "]");
+                
+                userInfoService.updateUser(newUserType, newUserName);
+                
+            }
+            else{
+                $scope.errors = "Error: " + output.err;
+                $scope.msgs = output.msg;
+                $scope.success = 2;
+                console.log($scope.errors);
+            }
+        }).error(function(output, status){
+            $scope.errors = "Status: " + status;
+            $scope.success = 2;
+            console.log($scope.errors);
+        });
+    }
+});
+
+
+app.controller('registerCtrl', function ($scope, $http, userInfoService) {
 
     //initializing....
     $scope.errors = '';
     $scope.msgs = '';
     $scope.formData = {};
+    $scope.formData.regyear = 2015;
     $scope.found = 0;
     $scope.showResults = false;
     $scope.foundPatron = false;
@@ -36,7 +142,9 @@ app.controller('registerCtrl', function ($scope, $http) {
     $scope.newpatron = true;
     $scope.title = "Register your patronage";
     $scope.service = '../api/pos.php';
-    $scope.admin = false; //for admin user functionality
+    $scope.user = {};
+    $scope.user.type = 'GUEST';
+    $scope.isAdminUser = false; //for admin user functionality
     $scope.formData.regyear = 2015;
     //console.log("Action:" + $scope.action);
     console.log("Service:" + $scope.service);
@@ -79,11 +187,13 @@ app.controller('registerCtrl', function ($scope, $http) {
         console.log("Selected Index = " +patronIndex);
         if ($scope.searchResults[patronIndex]) {
             $scope.formData = $scope.searchResults[patronIndex];
+            $scope.formData.regyear = 2015;
             $scope.showResults = false;
             $scope.foundPatron = true;
             console.log("Selected Patron ID = " + $scope.formData.id);
         } else {
             $scope.formData = {};
+            $scope.formData.regyear = 2015;
             $scope.errors = "ERROR: Unable to load selected patron - " + patronIndex;
             $scope.success = 2;
         }
@@ -95,7 +205,8 @@ app.controller('registerCtrl', function ($scope, $http) {
         $scope.errors = '';
         $scope.msgs = '';
         $scope.success = 0;
-
+        $scope.user = {};
+        $scope.user.type = 'GUEST';
         $scope.formData.action = 'register';
         console.log("Running submit registration...");
         console.log("Patron Id:", $scope.formData.id);
@@ -127,7 +238,7 @@ app.controller('registerCtrl', function ($scope, $http) {
     $scope.massageResults = function (field, data) {
         
         //ignore if admin user by forcing default case
-        if($scope.admin){field = "xx"};
+        if($scope.isAdminUser){field = "xx"};
         
         switch (field)
         {
@@ -169,5 +280,13 @@ app.controller('registerCtrl', function ($scope, $http) {
         $scope.foundPatron = false;
         $scope.newpatron = true;
     }
-});
+    
+    $scope.$on('userChanged', function(){
+        $scope.user.name = userInfoService.name;
+        $scope.user.type = userInfoService.type;
+        $scope.isAdminUser =  ($scope.user.type == 'ADMIN');
+        console.log("Trapping Event in registration controller");
+        console.log("User:" + $scope.user.name + "[" + $scope.user.type + "]");
 
+    });
+});
